1224d4ff9ca31a8345bde46006036a22d0f959ac
EDUEIRDO <cadusilvateno@gmail.com>
2024-04-22 20:19:30 -0300

    update

--- atividade3.js
+++ atividade3.js
@@ -2,8 +2,35 @@
 
 let altura = 20;
 let largura = 15;
-
-function calculo(altura, largura) {
-    resultado = altura * largura /2
-    return console.log(resultado)
+function calculo(a, b) {
+    return a * b /2
 }
+console.log(calculo(altura, largura))
+
+//Questão 2
+
+function verificador(){
+    let numero = 10;
+    if (numero % 2 ==0){
+        return true;
+    }else {
+        return false  
+    }
+}
+console.log(verificador())
+
+//Questão 3
+
+function convert(a){
+    return a * 2.6
+}
+
+console.log(convert(4))
+
+//Questão 4
+
+function imc(a, b){
+    return a / (b *2);
+
+}
+console.log(imc(80, 180))
